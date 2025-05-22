5135ab5e3b96fde646299f8450ba23c352715585
Nicolas <nicolas.parco@taligent.com.ar>
2025-05-22 16:27:24 -0300

    feat: :sparkles: Change axios client

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://api-portfolio-nparco.up.railway.app/api/v1",
+  baseURL: process.env.BACKEND_URL || "http://localhost:4000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
