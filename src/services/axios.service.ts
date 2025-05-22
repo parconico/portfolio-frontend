23c127a126f35b8086d29199c8539a342f87cf22
Nicolas <nicolas.parco@taligent.com.ar>
2025-05-22 16:34:36 -0300

    feat: :sparkles: last change axios url

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: "https://api-portfolio-nparco.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
